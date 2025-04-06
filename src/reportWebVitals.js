6adb0df2cc72098457815d4732cbf6f4ced96fb7
Lenisa Marisco <162896124+lenisamarisco@users.noreply.github.com>
2025-04-06 13:16:09 -0400

    changes

--- src/reportWebVitals.js
+++ src/reportWebVitals.js
@@ -1,4 +1,4 @@
-import { getCLS, getFID, getFCP, getLCP, getTTFB } from 'web-vitals';
+const { getCLS, getFID, getFCP, getLCP, getTTFB } = require('web-vitals');
 
 const reportWebVitals = (onPerfEntry) => {
   if (onPerfEntry && onPerfEntry instanceof Function) {
